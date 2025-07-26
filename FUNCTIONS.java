e4d696668319a129bf8b1613c9c2b52b0873c4a9
Ayush Deshmukh <deshmukhayush669@gmail.com>
2025-07-26 20:13:57 +0530

    code for functions

---
+++ FUNCTIONS.java
@@ -0,0 +1,9 @@
+public class FUNCTIONS {
+    public static void main(String [] args){
+        for(int i=0; i<5; i++){ 
+            AYUSH();
+        }
+    }
+   static void AYUSH() { System.out.println("hello");
+}
+}
