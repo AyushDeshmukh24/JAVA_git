8400325f03eb8347a9690a42caabccd025a4ad9c
Ayush Deshmukh <deshmukhayush669@gmail.com>
2025-07-27 11:56:42 +0530

    greeting

--- FUNCTIPON2.java
+++ FUNCTIPON2.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class FUNCTIPON2 {
+    public static void main(String[] args) {
+        System.out.println("enter your name");
+        Scanner in =new Scanner (System.in);
+        String nmae = in.next();
+        String amol = transformae(nmae);
+        System.out.println(amol);
+
+        
+    }
+    static String transformae(String nmae) {
+        String message = "hello"+ nmae;
+        return message;
+
+    }
     
 }
