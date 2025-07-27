132b3eb8471b66f2efb190dbe7b8ebfaae90b33a
Ayush Deshmukh <deshmukhayush669@gmail.com>
2025-07-27 11:36:16 +0530

    day 2

--- FUNCTIONS.java
+++ FUNCTIONS.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 public class FUNCTIONS {
     public static void main(String [] args){
-        for(int i=0; i<5; i++){ 
-            AYUSH();
-        }
-    }
-   static void AYUSH() { System.out.println("hello");
-}
+        int solution = AYUSh();
+        System.out.println(solution);
+      }
+      static int AYUSh(){
+        Scanner in = new Scanner (System.in);
+        System.out.println("Enter the first number that you want to add");
+        int num1 = in.nextInt();
+        System.out.println("enter the socund number");
+        int num2 = in.nextInt();
+        System.out.println("Sum of two number is =");
+        int sum = num1+num2;
+        return sum;
+
+
+
+      }
 }
