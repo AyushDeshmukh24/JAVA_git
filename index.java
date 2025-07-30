a26e0bf91bf004b75465f2de3b9ba908135d5cee
AyushDeshmukh24 <deshmukhayush669@gmail.com>
2025-07-30 10:50:14 +0530

    array

---
+++ index.java
@@ -0,0 +1,15 @@
+public class index {
+    public static void main(String[] args) {
+        int[] ayush = new int[5];
+        
+        ayush[0]=108;
+        ayush[1]=105;
+        ayush[2]=1025;
+        ayush[3]=102;
+        ayush[4]=100;
+
+        System.out.println(ayush[2]);
+        // here in the memory its stored as [108,105,1025,102,100]
+    }
+    
+}
